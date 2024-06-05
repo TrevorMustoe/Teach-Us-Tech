53f10e1d9acc20bdd97b9fbcd5233439a36cda34
Trevor Mustoe <trevormustoe0405@gmail.com>
2024-06-05 19:30:39 -0400

    created vocab cards using the firebase database

--- components/shared/domBuilder.js
+++ components/shared/domBuilder.js
@@ -3,8 +3,8 @@ import renderToDOM from '../../utils/renderToDom';
 const domBuilder = () => {
   const domString = `
   <div id="navigation"></div>
-  <div id="main-container">
-  </div>`;
+  <div id="main-container"></div>
+  <div id="store"></div>`;
 
   renderToDOM('#app', domString);
 };
--- utils/sample_data/startApp.js
+++ utils/sample_data/startApp.js
@@ -1,11 +1,12 @@
 import domBuilder from '../../components/shared/domBuilder';
 import navBar from '../../components/shared/navBar';
 import showVocab from '../../pages/vocab';
+import getVocab from '../../api/vocabData';
 
 const startApp = () => {
   domBuilder();
   navBar();
-  showVocab();
+  getVocab().then((vocabWords) => showVocab(vocabWords));
 };
 
 export default startApp;
