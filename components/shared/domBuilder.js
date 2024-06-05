3ab75465694aa582f5883f60a478e3760f8d3759
Trevor Mustoe <trevormustoe0405@gmail.com>
2024-06-05 16:19:33 -0400

    Added in sorting buttons

--- components/shared/domBuilder.js
+++ components/shared/domBuilder.js
@@ -2,7 +2,9 @@ import renderToDOM from '../../utils/renderToDom';
 
 const domBuilder = () => {
   const domString = `
-  <div id="navigation"></div>`;
+  <div id="navigation"></div>
+  <div id="main-container">
+  </div>`;
 
   renderToDOM('#app', domString);
 };
--- utils/sample_data/startApp.js
+++ utils/sample_data/startApp.js
@@ -1,9 +1,11 @@
 import domBuilder from '../../components/shared/domBuilder';
 import navBar from '../../components/shared/navBar';
+import showVocab from '../../pages/vocab';
 
 const startApp = () => {
   domBuilder();
   navBar();
+  showVocab();
 };
 
 export default startApp;
